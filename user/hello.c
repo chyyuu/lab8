4d82fe1a07f188e63eaa75acb7698405257f171c
chyyuu <yuchen@tsinghua.edu.cn>
2014-08-11 13:58:16 +0800

    modify hello.c for ucore_hookapitests plugin

--- user/hello.c
+++ user/hello.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <ulib.h>
 
+void print_hello(void) {
+    cprintf("print_hello: Hello world!!.\n");
+}
+void print_me(void) {
+    cprintf("print_me: I am process %d.\n", getpid());
+}
+void print_bye(void) {
+    cprintf("print_bye: \n");
+    cprintf("hello pass.\n");
+}
 int
 main(void) {
-    cprintf("Hello world!!.\n");
-    cprintf("I am process %d.\n", getpid());
-    cprintf("hello pass.\n");
+    cprintf("main: begin\n");
+    print_hello();
+    print_me();
+    print_bye();
     return 0;
 }
 
